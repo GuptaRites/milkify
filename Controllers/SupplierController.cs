19f60b7e54ccf136b2cedde0e57c380468240cac
GuptaRites <rk7256967552@gmail.com>
2025-03-30 18:09:21 +0530

    Added (detail, edit and delete) feature in Supplier

--- Controllers/SupplierController.cs
+++ Controllers/SupplierController.cs
@@ -57,5 +57,91 @@ namespace milkify.Controllers
             }
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var supplier = db.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Supplier sup)
+        {
+            if (id != sup.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Suppliers.Update(sup);
+                db.SaveChanges();
+                TempData["Succes"] = "Supplier updated successfully!";
+                return RedirectToAction("Index");
+            }
+
+            return View(sup);
+        }
+
+        public IActionResult Details(int id)
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var supplier = db.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            if (HttpContext.Session.GetString("UserName") == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var supplier = db.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var supplier = db.Suppliers.FirstOrDefault(s => s.Id == id);
+            if (supplier != null)
+            {
+                db.Suppliers.Remove(supplier);
+                db.SaveChanges();
+                TempData["Succes"] = "Supplier deleted.";
+            }
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
